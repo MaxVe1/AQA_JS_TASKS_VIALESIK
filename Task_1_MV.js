bd6700e0786ad6e304ce649b4c26c0f84c9f3b1d
100nout <emvi@tut.by>
2023-06-13 18:28:41 +0300

    Tasks 1-3 for Andersen : OK

---
+++ Task_1_MV.js
@@ -0,0 +1,12 @@
+function more7(n){
+    //n > 7 ? console.log('Привет') : console.log('Error');
+    return n > 7 ? 'Привет' : 'Error';
+}
+
+
+console.log(more7(11));
+
+
+console.log(more7(2));
+
+console.log(more7(10))
---
+++ Task_2_MV.js
@@ -0,0 +1,11 @@
+function isNameViacheslav(name){
+    //name === 'Вячеслав' ? console.log(`Привет, ${name}`) : console.log(`Нет такого имени`);
+    return name === 'Вячеслав' ? `Привет, ${name}` : `Нет такого имени`;
+}
+
+
+console.log(isNameViacheslav('Вячеслав'));
+
+console.log(isNameViacheslav('Sasha'));
+
+console.log(isNameViacheslav('Слава'));
